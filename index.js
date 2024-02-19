212af798347b86854ca1dabb3ed2f99e55d17dfa
Javed003fr <javed.ali003fr@gmail.com>
2024-02-19 23:47:21 +0600

    added some function

--- index.js
+++ index.js
@@ -8,14 +8,28 @@
 // }
 // alert()
 
-// function getSeatNumber(){
-//     const seatNumbers = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4', 'E1', 'E2', 'E3', 'E4', 'F1', 'F2', 'F3', 'F4', 'G1', 'G2', 'G3', 'G4', 'H1', 'H2', 'H3', 'H4']
-//     console.log(seatNumbers);
-// }
 
-// function seatSelect(){
-//     console.log("seat select korsi")
-// }
+    const seatNumbers = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4', 'E1', 'E2', 'E3', 'E4', 'F1', 'F2', 'F3', 'F4', 'G1', 'G2', 'G3', 'G4', 'H1', 'H2', 'H3', 'H4'];
+    console.log(seatNumbers);
+
+    const seats = document.getElementsByClassName('seat-btn');
+    let seatCount = 0;
+
+    for(let i =0; i < seatNumbers.length; i++){
+        const seatNum = seatNumbers[i];
+        const seat = seats[i];
+        seat.addEventListener("click", function(e){
+            if(seatCount < 4){
+                e.target.style.backgroundColor = '#1DD100';
+                seatCount++;
+            }
+            else{
+                alert('You are not allowed to book more than 4 seats.');
+            }
+            
+        })
+    }
+
 
 const allSeatBtn = document.getElementsByClassName('seat-btn');
 let count = 0;
@@ -23,25 +37,18 @@ let count = 0;
 for(const btn of allSeatBtn){
     btn.addEventListener("click", function(e){
         count = count + 1;
-
-        console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes)
-
         setInnerText('seat-count', count);
     })
 }
 
-function setInnerText(elementId, value){
-    document.getElementById(elementId).innerText = value;
+function setInnerText(e, value){
+    document.getElementById(e).innerText = value;
 }
 
-
-
-// const allSeats = document.getElementsByClassName("seat-left")
-// let availableSeats = 40;
-// for(const seat of allSeats){
-//     seat.addEventListener("click", function(elementId) {
-//         availableSeats -= 1;
-//         document.getElementById("total-seats").innerText = availableSeats;
-//         console.log(availableSeats);
-//     })
-// }
+function seatLeftCount(){
+    const leftSeat = document.querySelector('.seat-left');
+    const seatsLeft = parseInt(seatsLeft);
+    seatsLeft--;
+}
+const seatBtn = document.getElementsByClassName('seat-btn');
+seatBtn.addEventListener("click", seatLeftCount);
